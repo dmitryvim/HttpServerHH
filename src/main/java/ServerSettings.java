1f880e7be49f23742b26ce1ee474693e698c6407
mhty <olivergrane@gmail.com>
2016-03-10 06:08:32 +0300

    added xml work

--- src/main/java/ServerSettings.java
+++ src/main/java/ServerSettings.java
@@ -1,25 +1,69 @@
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-// /Users/mhty/IdeaProjects/HttpServer/server-settings/settings.xml
 class ServerSettings {
+    static private final String SERVER_NAME_KEY = "#document/server/name/";
+    static private final String SERVER_VERSION_KEY = "#document/server/version/";
+    static private final String HTTP_VERSION_KEY = "#document/server/http-version/";
+    static private final String HOME_DIRECTORY_KEY = "#document/server/home-directory/";
+    static private final String INDEX_FILE_KEY = "#document/server/index-file/";
+    static private final String PORT_KEY = "#document/server/port/";
+    static private final String CASH_TIMEOUT_KEY = "#document/server/file-cash-timeout/";
+    static private final String DIR_NOT_FOUND_KEY = "#document/server/system-page-path/not-found/";
+    static private final String DIR_BAD_REQUEST_KEY = "#document/server/system-page-path/bad-request/";
+
     private Map<String, String> attributes;
     private Document document;
     private char delimiter = '/';
 
-    static public void main(String[] args) {
-        ServerSettings ss = new ServerSettings("/Users/mhty/IdeaProjects/HttpServer/server-settings/settings.xml");
-        ss.init();
+    public String getAttr(String key) {
+        return attributes.get(key);
+    }
+
+    public String getServerName() {
+        return getAttr(SERVER_NAME_KEY);
+    }
+
+    public String getServerVersion() {
+        return getAttr(SERVER_VERSION_KEY);
+    }
+
+    public String getHttpVersion() {
+        return getAttr(HTTP_VERSION_KEY);
+    }
+
+    public String getHomeDirectory() {
+        return getAttr(HOME_DIRECTORY_KEY);
+    }
+
+    public String getIndexFile() {
+        return getAttr(INDEX_FILE_KEY);
+    }
+
+    public int getPort() {
+        return Integer.parseInt(getAttr(PORT_KEY));
+    }
+
+    public int getCashTimeout() {
+        return Integer.parseInt(getAttr(CASH_TIMEOUT_KEY));
+    }
+
+    public String getPathNotFound() {
+        return getAttr(DIR_NOT_FOUND_KEY);
+    }
+
+    public String getPathBadRequest() {
+        return getAttr(DIR_BAD_REQUEST_KEY);
+    }
+
+    public ServerSettings serverSettingsCreate(String filename) {
+        return new ServerSettings(filename).init();
     }
 
     private ServerSettings(String filename) {
@@ -33,8 +77,9 @@ class ServerSettings {
         }
     }
 
-    private void init() {
+    private ServerSettings init() {
         visit(document, "");
+        return this;
     }
 
     private void visit(Node node, String prefix) {
